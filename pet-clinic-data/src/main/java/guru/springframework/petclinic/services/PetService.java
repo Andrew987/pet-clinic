66622028b18a93967b356676d70a34ad8e15db28
Andras Urge <andris@mail.muni.cz>
2020-02-19 13:05:25 +0100

    Implement interfaces for services. Closes #4

---
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package guru.springframework.petclinic.services;
+
+import guru.springframework.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Set<Pet> findAll();
+
+    Pet save(Pet pet);
+}
